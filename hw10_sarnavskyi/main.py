88602842a2cdc35ab5b82f26167f95248c6b1223
Alex.Sarnavskiy <33147441+Chellsi@users.noreply.github.com>
2022-08-15 20:39:10 +0300

    version 1.0

--- hw10_sarnavskyi/main.py
+++ hw10_sarnavskyi/main.py
@@ -39,89 +39,95 @@ raise AttributeError(f'no parameter "password" in arguments of function{func.__n
 тут ви вже й самы здогадалися написати функцію на виконання даної роботи (тут вже без підказок)
 """
 
+keyword_args = {}
+result = {}
+
+
+def check_absence_password(*args, **kwargs):
+    keyword_args.update(kwargs)
+    count = False
+    for key in keyword_args.keys():
+        if key == 'password':
+            count = True
+    return count
+
+
+def is_valid_length(string, length=10):
+    count = False
+    if len(string) >= length:
+        count = True
+    return count
+
+
+def is_string(string):
+    count = False
+    if isinstance(string, str):
+        count = True
+    return count
+
+
+def has_any_symbols(string, method):
+    count = False
+    if method == 'letters_check':
+        for char in string:
+            if char.isalpha():
+                count = True
+        return count
+    elif method == 'contains_number':
+        for char in string:
+            if char.isdigit():
+                count = True
+        return count
+    elif method == 'contains_exclamation_mark':
+        for char in string:
+            if char == '!':
+                count = True
+        return count
+    return count
+
+
+def cut_result(to_check, *args, **kwargs):
+    func_res = str(to_check(*args, **kwargs))
+    if len(func_res) > 100:
+        return func_res[slice(97)] + '...'
+    else:
+        return func_res
+
 
 def wrap_validate(func):
-    keyword_args = {}
-    result = {}
-
-    def cut_result(*args, **kwargs):
-        func_res = str(func(*args, **kwargs))
-        print(func_res)
-        if len(func_res) > 100:
-            return func_res[slice(98)] + '...'
-        else:
-            return func_res
 
     def checker(*args, **kwargs):
         if check_absence_password(*args, **kwargs):
             pass
         else:
             raise AttributeError(f'no parameter "password" in arguments of function{func.__name__}')
-        if is_string():
+        if is_string(keyword_args['password']):
             pass
         else:
-            result.update({'result': cut_result(*args, **kwargs), 'is_secure': False})
+            result.update({'result': cut_result(func, *args, **kwargs), 'is_secure': False})
             return result
-        if is_valid_length(10):
+        if is_valid_length(keyword_args['password'], 10):
             pass
         else:
-            result.update({'result': cut_result(*args, **kwargs), 'is_secure': False})
+            result.update({'result': cut_result(func, *args, **kwargs), 'is_secure': False})
             return result
-        if has_any_symbols('letters_check'):
+        if has_any_symbols(keyword_args['password'], 'letters_check'):
             pass
         else:
-            result.update({'result': cut_result(*args, **kwargs), 'is_secure': False})
+            result.update({'result': cut_result(func, *args, **kwargs), 'is_secure': False})
             return result
-        if has_any_symbols('contains_number'):
+        if has_any_symbols(keyword_args['password'], 'contains_number'):
             pass
         else:
-            result.update({'result': cut_result(*args, **kwargs), 'is_secure': False})
+            result.update({'result': cut_result(func, *args, **kwargs), 'is_secure': False})
             return result
-        if has_any_symbols('contains_exclamation_mark'):
+        if has_any_symbols(keyword_args['password'], 'contains_exclamation_mark'):
             pass
         else:
-            result.update({'result': cut_result(*args, **kwargs), 'is_secure': False})
+            result.update({'result': cut_result(func, *args, **kwargs), 'is_secure': False})
             return result
-        result.update({'result': cut_result(*args, **kwargs), 'is_secure': True})
+        result.update({'result': cut_result(func, *args, **kwargs), 'is_secure': True})
         return result
-
-    def check_absence_password(*args, **kwargs):
-        keyword_args.update(kwargs)
-        count = False
-        for key in keyword_args.keys():
-            if key == 'password':
-                count = True
-        return count
-
-    def is_valid_length(length=10):
-        count = False
-        if len(keyword_args['password']) >= length:
-            count = True
-        return count
-
-    def is_string():
-        count = False
-        if isinstance(keyword_args['password'], str):
-            count = True
-        return count
-
-    def has_any_symbols(method):
-        count = False
-        if method == 'letters_check':
-            for char in keyword_args['password']:
-                if char.isalpha():
-                    count = True
-            return count
-        elif method == 'contains_number':
-            for char in keyword_args['password']:
-                if char.isdigit():
-                    count = True
-            return count
-        elif method == 'contains_exclamation_mark':
-            for char in keyword_args['password']:
-                if char == '!':
-                    count = True
-            return count
 
     return checker
 
@@ -155,8 +161,7 @@ def registration(id, /, login=None, notes=None, *, password=None):
     date = datetime.date.today()
     return str(f'User {login} created account on {date} with password "{password}". Additional information: {notes}')
 
-
-
+print(registration(12, password=None))
 
 ##############################################################################
 ############                                                     #############
@@ -177,8 +182,40 @@ def registration(id, /, login=None, notes=None, *, password=None):
 """
 
 if __name__ == '__main__':
-    assert type(registration(3434, 'Comp', notes='notes', password='wasdwasd9!')) == dict, 'not a string'
-    assert type(registration(3434, 'Comp', notes='notes', password='wasdwasd9')) == dict, 'not a dict'
+    assert check_absence_password(password=None, username='gus')
+    assert check_absence_password(password='qweqweqwe1!')
+    assert check_absence_password(password=12)
+
+    assert not is_valid_length('1234', 5)
+    assert not is_valid_length('1234')
+    assert is_valid_length('1234', 4)
+
+    assert not is_string(1)
+    assert not is_string({1: 2})
+    assert is_string('string')
+
+    assert not has_any_symbols('1!', 'letters_check')
+    assert not has_any_symbols('s!', 'contains_number')
+    assert not has_any_symbols('1s', 'contains_exclamation_mark')
+    assert has_any_symbols('s', 'letters_check')
+
+    assert cut_result(lambda x: 'a' * 101, 1) == 'a' * 97 + '...'
+    assert cut_result(lambda x: 'a' * 100, 1) == 'a' * 100
+    assert cut_result(lambda x: 'a' * 50, 1) == 'a' * 50
+
+    assert registration(12, 'fff', 'nnn', password='asdasdasd1!') == \
+           {'result': 'User fff created account on 2022-08-15 with password "asdasdasd1!". '
+                      'Additional information: nnn', 'is_secure': True}
+    assert type(registration(12, 'fff', 'nnn', password='')) == dict
+    assert registration(12, 'fff', 'nnn', password='asdasdasd1')
+    assert registration(12, password=None)['is_secure'] is False
+    assert registration(12, 'fff', 'nnn', password='a')['is_secure'] is False
+    assert registration(12, 'fff', 'nnn', password='aaaaaaaaaa1')['is_secure'] is False
+    assert registration(12, 'fff', 'nnn', password='aaaaaaaaaa!')['is_secure'] is False
+    assert registration(12, 'fff', 'nnn', password='aaaaaaa1!')['is_secure'] is False
+    assert registration(12, 'fff', 'nnn', password='aaaaaaaaaa1!')['is_secure'] is True
+
+
 
 
 ##############################################################################
